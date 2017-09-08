186dd0547f63e9eacc23375b697c0db6ad697516
zhangpeng130 <zp13022188023@163.com>
2017-09-08 16:03:25 +0800

    zhangpeng130 4

--- goods/src/test/java/goods/Zhangpeng130.java
+++ goods/src/test/java/goods/Zhangpeng130.java
@@ -8,6 +8,6 @@ public static void main(String[] args) {
 	System.out.println("123456");
 	System.out.println("2345");
 	System.out.println("3456");
-	System.out.println("4567");
+	System.out.println("45678");
 }
 }
