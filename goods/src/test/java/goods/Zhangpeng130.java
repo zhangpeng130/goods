9645bb3e2f8c8826928c84394ecacd79e42687dd
pengzhangGit <z13022188023@163.com>
2017-09-08 15:49:49 +0800

    txxx

--- goods/src/test/java/goods/Zhangpeng130.java
+++ goods/src/test/java/goods/Zhangpeng130.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 	System.out.println("zhangpeng130 commit fork");
 	System.out.println("zhangpeng130");
 	System.out.println("123");
+	System.out.println("123456");
 }
 }
