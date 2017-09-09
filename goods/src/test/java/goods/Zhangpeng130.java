d15fa0f3fcc9f916cb33d2fb43f240b08ddab6d2
zhangpeng130 <zp13022188023@163.com>
2017-09-10 01:58:36 +0800

    130 commit

--- goods/src/test/java/goods/Zhangpeng130.java
+++ goods/src/test/java/goods/Zhangpeng130.java
@@ -5,9 +5,6 @@ public static void main(String[] args) {
 	System.out.println("zhangpeng130 commit fork");
 	System.out.println("zhangpeng130");
 	System.out.println("123");
-	System.out.println("123456");
-	System.out.println("2345");
-	System.out.println("3456");
-	System.out.println("45678");
+
 }
 }
