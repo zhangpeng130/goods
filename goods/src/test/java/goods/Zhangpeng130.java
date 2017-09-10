297c6d9094737d41ef7f0562fc682f093cb5735e
zhangpeng130 <zhangpeng130@users.noreply.github.com>
2017-09-10 16:31:54 +0800

    Update Zhangpeng130.java
    upstram master

--- goods/src/test/java/goods/Zhangpeng130.java
+++ goods/src/test/java/goods/Zhangpeng130.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("zhangpeng130 commit fork");
 	System.out.println("zhangpeng130");
 	System.out.println("123");
+	sysout.pring(remote upstream master);
 
 }
 }
