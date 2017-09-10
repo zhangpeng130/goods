126544925ee69fa56621352b752b76eb99abe56a
zhangpeng130 <zp13022188023@163.com>
2017-09-10 21:02:29 +0800

    seconds commit code

--- goods/src/test/java/goods/Zhangpeng130.java
+++ goods/src/test/java/goods/Zhangpeng130.java
@@ -6,6 +6,7 @@ public static void main(String[] args) {
 	System.out.println("zhangpeng130");
 	System.out.println("123");
 	System.out.println("update eror code  from coolyDev");
+	System.out.println("seconds update eoor code from coolyDev");
 
 }
 }
