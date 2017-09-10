e0a6cc76aa6f80baef8cc6656c039b58113420d3
zhangpeng130 <zp13022188023@163.com>
2017-09-10 17:36:16 +0800

    coolyDev

--- goods/src/test/java/goods/Zhangpeng130.java
+++ goods/src/test/java/goods/Zhangpeng130.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("zhangpeng130 commit fork");
 	System.out.println("zhangpeng130");
 	System.out.println("123");
-	sysout.pring(remote upstream master);
+	System.out.println("update eror code  from coolyDev");
 
 }
 }
