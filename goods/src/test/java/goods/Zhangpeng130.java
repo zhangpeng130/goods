184cee33eb51bd5652739a61c55840a6e1967826
zhangpeng130 <zp13022188023@163.com>
2017-09-10 23:53:51 +0800

    seven update

--- goods/src/test/java/goods/Zhangpeng130.java
+++ goods/src/test/java/goods/Zhangpeng130.java
@@ -11,6 +11,7 @@ public static void main(String[] args) {
 	System.out.println("four  update error");
 	System.out.println("five update error code");
 	System.out.println("six update");
+	System.out.println("seven update");
 
 }
 }
