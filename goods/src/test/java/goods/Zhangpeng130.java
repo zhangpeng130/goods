f6d2e4cfb833c08e517f90be9957243c856d1458
zhangpeng130 <zp13022188023@163.com>
2017-11-19 17:31:17 +0800

    6

--- goods/src/test/java/goods/Zhangpeng130.java
+++ goods/src/test/java/goods/Zhangpeng130.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
 	System.out.println("zhangpeng130 commit fork");
 	System.out.println("zhangpeng130");
 	System.out.println("123");
-	System.out.println("2017年11月19日16:54:33 开始销管项目5");
+	System.out.println("2017年11月19日16:54:33 开始销管 项目5");
 	
 
 }
