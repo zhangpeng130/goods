bc889a03372df49fb3ee858ed7481ac94c9f831c
zhangpeng130 <zp13022188023@163.com>
2017-12-10 15:26:35 +0800

    新增POI导入pom配置及Demo

--- goods/src/main/java/com/yiren/entity/UserInfo.java
+++ goods/src/main/java/com/yiren/entity/UserInfo.java
@@ -2,30 +2,35 @@ package com.yiren.entity;
 
 import java.io.Serializable;
 import java.sql.Date;
+
 /**
  * 用户信息
+ * 
  * @author peng
  *
  */
-public class UserInfo implements Serializable{
+public class UserInfo implements Serializable {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	private long id;	
+	private long id;
+
 	public long getId() {
 		return id;
 	}
-	public void setId(long id) {	
+
+	public void setId(long id) {
 		this.id = id;
 	}
+
 	/**
 	 * 用户名
 	 */
-private String userName;
-/**
- *密码
- */
+	private String userName;
+	/**
+	 * 密码
+	 */
 	private String password;
 	/**
 	 * 昵称
@@ -42,62 +47,70 @@ private String userName;
 	private String status;
 
 	private Date loginTime;;
-	private Date createTime;	
-	
+	private Date createTime;
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getNickName() {
 		return nickName;
 	}
+
 	public void setNickName(String nickName) {
 		this.nickName = nickName;
 	}
+
 	public String getRole() {
 		return role;
 	}
+
 	public void setRole(String role) {
 		this.role = role;
 	}
+
 	public String getStatus() {
 		return status;
 	}
+
 	public void setStatus(String status) {
 		this.status = status;
 	}
+
 	public Date getLoginTime() {
 		return loginTime;
 	}
+
 	public void setLoginTime(Date loginTime) {
 		this.loginTime = loginTime;
 	}
-	
-	
+
 	public String getPhone() {
 		return phone;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
-	
-	
-	
 
-	public Date getCreateTime() {	
+	public Date getCreateTime() {
 		return createTime;
 	}
+
 	public void setCreateTime(Date createTime) {
 		this.createTime = createTime;
 	}
-		
 
 }
--- goods/src/main/java/com/yiren/service/impl/UserInfoServiceimpl.java
+++ goods/src/main/java/com/yiren/service/impl/UserInfoServiceimpl.java
@@ -10,39 +10,38 @@ import com.yiren.dao.UserInfoDao;
 import com.yiren.entity.UserInfo;
 import com.yiren.service.UserInfoService;
 import com.yiren.utils.MD5Utils;
+
 @Service
-public class UserInfoServiceimpl implements UserInfoService{
-@Resource
+public class UserInfoServiceimpl implements UserInfoService {
+	@Resource
 	private UserInfoDao userInfoDao;
 
-@Override
-public int addUserInfo(UserInfo userInfo) {
-	int count=userInfoDao.insertUserInfo(userInfo);
-	return count;
-}
-
-@Override
-public List<UserInfo> findUserInfoByUserName(UserInfo userInfo) {
-	List<UserInfo> userInfoList= userInfoDao.findUserInfoByUserName(userInfo);
-	return userInfoList;
-}
-
-@Override
-public boolean login(String userName, String password) {	
-	UserInfo userInfo =new UserInfo();
-	userInfo.setUserName(userName);
-	List<UserInfo> userInfoList=findUserInfoByUserName( userInfo);
-	if(userInfoList!=null&&userInfoList.size()>0){
-		UserInfo userInfoFind=userInfoList.get(0);
-		String passwordFind=userInfoFind.getPassword();
-		 MD5Utils md5 = new MD5Utils();  
-		 boolean result=md5.validatePassword(passwordFind, password);
-		 return result;
+	@Override
+	public int addUserInfo(UserInfo userInfo) {
+		int count = userInfoDao.insertUserInfo(userInfo);
+		return count;
 	}
-	return false;
-}
 
+	@Override
+	public List<UserInfo> findUserInfoByUserName(UserInfo userInfo) {
+		List<UserInfo> userInfoList = userInfoDao
+				.findUserInfoByUserName(userInfo);
+		return userInfoList;
+	}
 
-	
+	@Override
+	public boolean login(String userName, String password) {
+		UserInfo userInfo = new UserInfo();
+		userInfo.setUserName(userName);
+		List<UserInfo> userInfoList = findUserInfoByUserName(userInfo);
+		if (userInfoList != null && userInfoList.size() > 0) {
+			UserInfo userInfoFind = userInfoList.get(0);
+			String passwordFind = userInfoFind.getPassword();
+			MD5Utils md5 = new MD5Utils();
+			boolean result = md5.validatePassword(passwordFind, password);
+			return result;
+		}
+		return false;
+	}
 
 }
